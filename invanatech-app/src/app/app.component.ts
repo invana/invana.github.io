18d43e0df2c194c38eb93685df5bf1bd741a30d6
Ravi RT Merugu <rrmerugu@gmail.com>
2018-04-29 04:35:34 +0530

    UI text updateds

--- invanatech-app/src/app/app.component.ts
+++ invanatech-app/src/app/app.component.ts
@@ -10,12 +10,34 @@ export class AppComponent {
   email_id = "hello@invanatech.com";
 
   showContact() {
-    var btn = document.getElementById('contact-details');
+    let btn = document.getElementById('contact-details');
     btn.innerHTML = "Contact us " + this.email_id + " or +91 9052053549";
     window.location.href = "mailto:" + this.email_id;
   }
+
   scroll(el_id) {
-  document.getElementById( el_id).scrollIntoView();
-    window.location.hash =  el_id;
-}
+    document.getElementById(el_id).scrollIntoView();
+    window.location.hash = el_id;
+  }
+
+  // generateRandomInteger(min, max) {
+  //   return Math.floor(min + Math.random() * (max + 1 - min))
+  // }
+  //
+  // randomPulseGenerator() {
+  //   /* this method will start injecting pulse effects everywhere in the banner backgroud */
+  //   console.log("randomPulseGenerator triggered");
+  //   var node = document.createElement("span");
+  //   node.className = ('pulse');
+  //   node.style = ("backgroud:red")
+  //   node.id = ('generated-pulse');
+  //
+  //   let banner_el = document.getElementById('homepage-banner');
+  //   banner_el.appendChild(node);
+  // }
+  //
+  // ngAfterViewInit() {
+  //   console.log('Calendar Loaded');
+  //   this.randomPulseGenerator();
+  // }
 }
