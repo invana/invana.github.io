283c1a90e50d0b832737c5d1f392e5c7ad853460
Ravi RT Merugu <rrmerugu@gmail.com>
2018-04-29 03:56:13 +0530

    UI text updateds

--- invanatech-app/src/app/app.component.ts
+++ invanatech-app/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Invanatech';
+  email_id = "hello@invanatech.com";
+
+  showContact() {
+   var btn = document.getElementById('contact-details');
+   btn.innerHTML = "Contact us " + this.email_id +" or +91 9052053549";
+   window.location.href= "mailto:" + this.email_id ;
+  }
 }
